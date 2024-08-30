1d0afc1ca1111a2dbc10c78d04eff35d1c56728d
Alexey Beazrebriy <lohanecapho2000@gmail.com>
2024-08-30 15:28:04 +0200

    Fixed token 2

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -9,6 +9,6 @@ export const generateTokenAndSetCookie = async (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "none",
-    secure: process.env.NODE_ENV !== "development",
+    // secure: process.env.NODE_ENV !== "development",
   })
 }
